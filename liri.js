e01d4b614c14c1020fdc720fa4c85d3e9a3e56bc
Tiff L <tifftiffnlewis@gmail.com>
2019-02-12 06:10:33 -0600

    Updated the format of information returned

--- liri.js
+++ liri.js
@@ -16,16 +16,15 @@ var userSearchItem = process.argv.slice(3);
 
 var userSearch = userSearchItem.join('+');
 
-var bitURL = 'https://rest.bandsintown.com/artists/' + userSearch + '/event?app_id=codingbootcamp';
+var bitURL = 'https://rest.bandsintown.com/artists/' + userSearch + '/events?app_id=codingbootcamp';
 
-var omdbURL = 'http://www.omdbapi.com/?t=' + userSearch + '&y=&plot=short&apikey=trilogy';
+var omdbUrl = 'http://www.omdbapi.com/?t=' + userSearch + '&y=&plot=short&apikey=trilogy';
 
 //spotify
 
 function searchSpotify() {
 
     var keys = require('./keys.js');
-    console.log(keys);
 
     var spotify = new Spotify(keys.spotify);
 
@@ -40,7 +39,7 @@ function searchSpotify() {
         if (err) {
             return console.log('Error Occurred: ' + err);
         }
-        console.log('\n       ' + data.tracks.items[0].artists[0].name + ' - < ' + data.tracks.items[0].name + ' >');
+        console.log('\n' + data.tracks.items[0].artists[0].name + ' - < ' + data.tracks.items[0].name + ' >');
         console.log('\n------------------Song Info--------------------------------')
         console.log('Artist: ' + data.tracks.items[0].artists[0].name);
         console.log('Album Name: ' + data.tracks.items[0].album.name);
@@ -54,12 +53,12 @@ function searchSpotify() {
 function bitSearch() {
     axios.get(bitURL).then(
         function (response) {
-            console.log('\n    ' + response.data[0].lineup);
-            console.log('\n ---------------Show Info-------------------------------- ');
+            console.log('\n' + response.data[0].lineup);
+            console.log('\n----------------Show Info---------------------------------');
             console.log('Venue: ' + response.data[0].venue.name);
-            console.log('Location: ' + response.data[0].venue.city + ', ' + response.data[0].venue.country);
-            console.log('Date: ' + moment(response.data[0].datetime).format('L') + ' (MM/DD/YYYY)');
-            console.log('-------------------------------------------------------')
+            console.log('Location: ' + response.data[0].venue.city + ', ' + response.data[0].venue.region + ', ' + response.data[0].venue.country);
+            console.log('Date: ' + moment(response.data[0].datetime).format('L'));
+            console.log('------------------------------------------------------------')
         }
     );
 }
@@ -67,15 +66,15 @@ function bitSearch() {
 //OMDB
 
 function omdbSearch (){
-    axios.get(omdbURL).then(
+    axios.get(omdbUrl).then(
         function (response) {
-            console.log('\n    ' + response.data.Title + '  (' + response.data.Year + ')');
-            console.log('\n --------------------Movie Info-----------------------------');
+            console.log('\n' + response.data.Title + '  (' + response.data.Year + ')');
+            console.log('\n---------------------Movie Info-----------------------------');
             console.log('IMDB Rating: ' + response.data.imdbRating);
             console.log('Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value);
             console.log('Country: ' + response.data.Country);
             console.log('Language: ' + response.data.Language);
-            console.log(' Actors: ' + response.data.Actors);
+            console.log('Actors: ' + response.data.Actors);
             console.log('Plot: ' + response.data.Plot);
             console.log('------------------------------------------------------------');
         }
@@ -88,13 +87,14 @@ if (userRequest === "spotify-this") {
     searchSpotify();
 }
 else if (userRequest === "concert-this") {
+    console.log(bitURL);
     bitSearch();
 }
 else if (userRequest === "movie-this" && userSearchItem.length > 0) {
     omdbSearch();
 }
 else if (userRequest === "movie-this" && !userSearchItem.length > 0) {
-    omdbUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy"
+    omdbUrl = "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=trilogy"
     omdbSearch();
 }
 else if (userRequest === "do-what-it-says") {
